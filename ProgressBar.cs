5148403b332f5d6f48953b425d7c501a9cc2e1d6
Viswamedha Nalabotu <nalabotuviswamedha@gmail.com>
2025-03-07 19:58:42 +0000

    Current dashboard layout

---
+++ ProgressBar.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskFlow
+{
+    public class ProgressBar
+    {
+        private int totalProgress;
+        private int currentProgress;
+
+        public ProgressBar(int totalProgress)
+        {
+            this.totalProgress = totalProgress;
+            this.currentProgress = 0;
+        }
+
+        public void Load(int percentMore)
+        {
+            currentProgress += percentMore;
+            if (currentProgress >= totalProgress)
+            {
+                ProgressComplete?.Invoke(this, EventArgs.Empty);
+            }
+        }
+
+        public event EventHandler ProgressComplete;
+    }
+}
---
+++ Views/Dashboard.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace TaskFlow.Views
+{
+    /// <summary>
+    /// Interaction logic for Dashboard.xaml
+    /// </summary>
+    public partial class Dashboard : Page
+    {
+        ProgressBar bar = new ProgressBar(100);
+
+        public string Value { get; set; }
+        public Dashboard()
+        {
+            InitializeComponent();
+            bar.ProgressComplete += OnProgressComplete;
+        }
+
+        static void OnProgressComplete(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            bar.Load(10);
+        }
+    }
+}
